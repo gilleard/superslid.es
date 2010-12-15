0331bcb6bb75516af6ce4adaf7b786be99a08fe1
Adam Gilleard <adam@gilleard.co.uk>
2010-12-15 13:30:30 +0000

    Upgraded 1.5D with transitions and improved controls.
    Amended syntax since extra wrapper was no longer being used.
    Scales with browser.
    Switched arrows controls around to match new layout.
    Support for up and down sub slides (default down, add .up to title slide to switch)
    Fixed overview after the transitions broke it.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ function superSlides(options) {
   var settings = {
     width: 980,
     height: 735,
-    fontSize: 150
+    scale: 0.9,
+    fontSize: 150,
+    enableMath: false,
+    strictNav: false
   };
 
   // Update settings with user's values
@@ -41,18 +44,27 @@ function superSlides(options) {
   // Pointers
   var
     $g_body = $('body'),
-    $g_wrapper = $g_body.find('> .wrapper'),
-    $g_slides = $g_wrapper.find('> #slides > div'),
+    $g_wrapper = $g_body.find('#slides'),
+    $g_slides = $g_wrapper.find('> div'),
     $g_currentSlide = null,
-    $g_overview = null;
+    $g_overview = $('#overview');
 
   // General variables
   var
     g_totalSlides = $g_slides.size(),
     g_currentSlideNumber = isNaN(location.hash.substr(1)) || location.hash === '' ? 1 : location.hash.substr(1),
-    g_currentView = isNaN(location.hash.substr(1)) || location.hash === '' ? e_view.outline : e_view.slides,
+    g_currentView = e_view.slides,
     g_skipSlides = '',
     g_overviewActive = false;
+    
+  // Scale/position
+  var
+    g_windowWidth = null,
+    g_windowHeight = null,
+    g_wrapperWidth = null,
+    g_wrapperHeight = null,
+    g_wrapperHorizontalMargin = null,
+    g_wrapperVerticalMargin = null;
 
   // **************************************************
   //
@@ -69,42 +81,115 @@ function superSlides(options) {
   } // log(message)
   
   function scaleSlides() {
-  
-    var
-      l_windowWidth = $(window).width(),
-      l_windowHeight = $(window).height();
+
+    g_windowWidth = $(window).width();
+    g_windowHeight = $(window).height();
     
     // Wider than slide ratio
-    if((l_windowWidth/l_windowHeight) > (settings.width/settings.height)) {
+    if((g_windowWidth/g_windowHeight) > (settings.width/settings.height)) {
       
-      // Limit width, auto centered
-      var l_wrapperWidth = parseInt(l_windowHeight * (settings.width/settings.height), 10);
+      // Width set to settings.scale% of windows height
+      g_wrapperHeight = settings.scale * g_windowHeight;
       
-      $g_wrapper.css({
-        'width': l_wrapperWidth + 'px',
-        'height': l_windowHeight + 'px',
-        'margin-top': '0px',
-        'font-size': (l_wrapperWidth / settings.width) * settings.fontSize + '%'
-      });
+      // Height based on width to preserve aspect ratio
+      g_wrapperWidth = g_wrapperHeight * (settings.width/settings.height);
       
     }
     
     // Taller than slide ratio
     else {
-    
-      // Limit height, calculate margin to centre
-      var l_wrapperHeight = parseInt(l_windowWidth * (settings.height/settings.width), 10);
-    
-      $g_wrapper.css({
-        'width': l_windowWidth + 'px',
-        'height': l_wrapperHeight + 'px',
-        'margin-top': parseInt((l_windowHeight - l_wrapperHeight) / 2, 10) + 'px',
-        'font-size': (l_wrapperHeight / settings.height) * settings.fontSize + '%'
-      });
+      
+      // Height set to settings.scale% of windows width
+      g_wrapperWidth = settings.scale * g_windowWidth;
+      
+      // Width based on height to preserve aspect ratio
+      g_wrapperHeight = g_wrapperWidth * (settings.height/settings.width);
       
     }
+      
+    // Centre slide
+    g_wrapperHorizontalMargin = (g_windowWidth - g_wrapperWidth) / 2;
+    g_wrapperVerticalMargin = (g_windowHeight - g_wrapperHeight) / 2;
+    
+    // Scale slides wrapper
+    $g_wrapper.css({
+      'width': parseInt(g_wrapperWidth, 10) + 'px',
+      'height': parseInt(g_wrapperHeight, 10) + 'px',
+      'margin': parseInt(g_wrapperVerticalMargin, 10) + 'px ' + parseInt(g_wrapperHorizontalMargin, 10) + 'px',
+      'font-size': (settings.fontSize * (g_wrapperWidth / settings.width)) + '%'
+    });
+    
+    if($g_overview.size() > 0) {
+    
+      // Scale overview
+      $g_overview.find('> div').css({
+        'width': parseInt(g_wrapperWidth / 2, 10) + 'px',
+        'height': parseInt(g_wrapperHeight, 10) + 'px',
+        'margin': parseInt(g_wrapperVerticalMargin, 10) + 'px auto',
+        'font-size': (settings.fontSize * (g_wrapperWidth / settings.width)) + '%'
+      });
+    
+    }
+  
+    // Position slides
+    positionSlides();
   
   } // scaleSlides()
+  
+  function positionSlides() {
+  
+    // Init positioning variables
+    var
+      l_nonSubSlideIndex = 0,
+      l_subSlideIndex = 0,
+      l_subSlideGoingDown = true,
+      l_topOffsetPerSlide = parseInt(g_wrapperHeight + (g_wrapperVerticalMargin / 2), 10),
+      l_leftOffsetPerSlide = parseInt(g_wrapperWidth + (g_wrapperHorizontalMargin / 2), 10);
+  
+    $g_slides.not(':first').each(function() {
+      
+      // Non-sub slide
+      if(!$(this).hasClass('sub')) {
+      
+        // Increment non-sub slide index
+        l_nonSubSlideIndex += 1;
+        
+        // Reset sub slide index
+        l_subSlideIndex = 0;
+        
+        // Update the sub slide direction
+        l_subSlideGoingDown = $(this).hasClass('up');
+        
+      }
+      
+      // Sub slide 
+      else {
+      
+        // Increment sub slide index
+        l_subSlideIndex += 1;
+        
+      }
+      
+      // Position slide accordingly
+      $(this).css({
+        'top': l_subSlideIndex > 0 ? (l_subSlideGoingDown ? '-' : '') + (l_subSlideIndex * l_topOffsetPerSlide) + 'px' : 'auto',
+        'left': l_nonSubSlideIndex > 0 ? (l_nonSubSlideIndex * l_leftOffsetPerSlide) + 'px' : 'auto'
+      });
+      
+    });
+    
+    // Centre current slide
+    if($g_currentSlide) {
+    
+      // Move to new slide
+      $g_body.css({
+        'top': $g_currentSlide.position().top * -1 + 'px',
+        'left': $g_currentSlide.position().left * -1 + 'px'
+      });
+    
+    }
+  
+  } // positionSlides()
 
   function goToSlide(l_slideNumber, l_direction) {
 
@@ -128,21 +213,14 @@ function superSlides(options) {
     // Update variables
     $g_currentSlide = $g_slides.eq(l_slideNumber - 1);
     g_currentSlideNumber = l_slideNumber;
-
-    // Prepare incremented elements
-    switch(l_direction) {
-
-      case e_direction.forward:
-        $g_currentSlide.find('.incremental').hide().attr('data-visibility', 'hidden');
-        break;
-
-      case e_direction.back:
-        $g_currentSlide.find('.incremental').show().attr('data-visibility', 'visible');
-        break;
-
-    } // goToSlide(l_slideNumber, l_direction)
-
-    // Show new slide
+    
+    // Move to new slide
+    $g_body.css({
+      'top': $g_currentSlide.position().top * -1 + 'px',
+      'left': $g_currentSlide.position().left * -1 + 'px'
+    });
+    
+    // Add class to current slide
     $g_currentSlide.addClass('current');
 
     // Update url
@@ -150,39 +228,53 @@ function superSlides(options) {
       location.hash = '#' + g_currentSlideNumber;
     }
 
-  }
+  } // goToSlide()
 
   function advance(l_dimension) {
 
     // Get elements to be incremented
-    var $l_elementsToIncrement = $g_currentSlide.find('.incremental[data-visibility=hidden]');
+    var $l_elementsToIncrement = $g_currentSlide.find('.incremental:not(:visible)');
 
     // If there are some
     if($l_elementsToIncrement.size() > 0) {
 
       // Show the next one
       log('Showing next element');
-      $l_elementsToIncrement.first().show().attr('data-visibility', 'visible');
+      $l_elementsToIncrement.first().show();
 
     }
 
     // If we're not at the end of the slides
     else if(g_currentSlideNumber < g_totalSlides) {
 
-      // If trying to view the next sub slide and the next slide is a sub slide
-      if((l_dimension == e_dimension.sub) && ($g_currentSlide.next().hasClass('sub'))) {
+      // Trying to view next sub slide
+      if(l_dimension == e_dimension.sub) {
+      
+        // Next slide is a sub slide
+        if($g_currentSlide.next().hasClass('sub')) {
+        
+          // Go!
           goToSlide(parseInt(g_currentSlideNumber, 10) + 1, e_direction.forward);
-          return;
-      }
+          
+        }
+        
+      } // l_dimension == e_dimension.sub
+      
+      // Trying to view next non-sub slide
+      else {
 
-      // Find the next non-sub slide
-      var $l_nextTitleSlide = $g_currentSlide.nextAll(':not(.sub)');
-      if($l_nextTitleSlide.size() > 0) {
-
-        // Skip to the next non-sub slide
-        goToSlide(parseInt($g_slides.index($l_nextTitleSlide.first()), 10) + 1, e_direction.forward);
-
-      }
+        // Find the next non-sub slide
+        var $l_nextTitleSlide = $g_currentSlide.nextAll(':not(.sub)');
+        
+        // If there is one and strictNav is off or this is a non-sub slide
+        if($l_nextTitleSlide.size() > 0 && (!settings.strictNav || !$g_currentSlide.hasClass('sub'))) {
+  
+          // Skip to the next non-sub slide
+          goToSlide(parseInt($g_slides.index($l_nextTitleSlide.first()), 10) + 1, e_direction.forward);
+  
+        }
+      
+      } // l_dimension == e_dimension.title
 
     } // g_currentSlideNumber < g_totalSlides
 
@@ -196,34 +288,48 @@ function superSlides(options) {
   function previous(l_dimension) {
 
     // Get elements already incremented
-    var $l_elementsIncremented = $g_currentSlide.find('.incremental[data-visibility=visible]');
+    var $l_elementsIncremented = $g_currentSlide.find('.incremental:visible');
 
     // If there are some
     if($l_elementsIncremented.size() > 0) {
 
       // Hide the last one
       log('Hiding next element');
-      $l_elementsIncremented.last().hide().attr('data-visibility', 'hidden');
+      $l_elementsIncremented.last().hide();
 
-    }
+    } // $l_elementsIncremented.size() > 0
 
     // If we're not at the start of the slides
     else if(g_currentSlideNumber > 1) {
 
-      // If trying to view the prev sub slide and the prev slide is a sub slide
-      if((l_dimension == e_dimension.sub) && (!$g_currentSlide.prev().hasClass(':not(sub)'))) {
+      // Trying to view the prev sub slide
+      if(l_dimension == e_dimension.sub) {
+      
+        // This is a sub slide
+        if($g_currentSlide.hasClass('sub')) {
+        
+          // Go!
           goToSlide(parseInt(g_currentSlideNumber, 10) - 1, e_direction.back);
-          return;
-      }
+        
+        }
+        
+      } // l_dimension == e_dimension.sub
+      
+      // Trying to view prev non-sub slide
+      else {
 
-      // Find the prev non-sub slide
-      var $l_prevTitleSlide = $g_currentSlide.prevAll(':not(.sub)');
-      if($l_prevTitleSlide.size() > 0) {
-
-        // Skip to the prev non-sub slide
-        goToSlide(parseInt($g_slides.index($l_prevTitleSlide.first()), 10) + 1, e_direction.back);
-
-      }
+        // Find the prev non-sub slide
+        var $l_prevTitleSlide = $g_currentSlide.prevAll(':not(.sub)');
+        
+        // If there is one and strictNav is off or this is a non-sub slide
+        if($l_prevTitleSlide.size() > 0 && (!settings.strictNav || !$g_currentSlide.hasClass('sub'))) {
+  
+          // Skip to the prev non-sub slide
+          goToSlide(parseInt($g_slides.index($l_prevTitleSlide.first()), 10) + 1, e_direction.back);
+  
+        }
+      
+      } // l_dimension == e_dimension.title
 
     } // g_currentSlideNumber > 1
 
@@ -254,17 +360,6 @@ function superSlides(options) {
     
       // Check for resize
       scaleSlides();
-    
-    }
-    else {
-    
-      // Remove any scaling
-      $g_wrapper.css({
-        'width': 'auto',
-        'height': 'auto',
-        'margin-top': '0px',
-        'font-size': 'inherit'
-      });
     
     }
     
@@ -326,7 +421,7 @@ function superSlides(options) {
   function generateFooters() {
   
     // Copy footer into each slide
-    $g_slides.append($('body > .wrapper > footer'));
+    $g_slides.append($g_wrapper.find('> footer'));
     
     // For each slide
     $g_slides.each(function() {
@@ -340,9 +435,28 @@ function superSlides(options) {
     $g_slides.find('.total').text(g_totalSlides);
   
   } // generateFooters()
+  
+  function initMathJax() {
+  
+    var l_scriptTag = document.createElement("script");
+    l_scriptTag.type = "text/javascript";
+    l_scriptTag.src = "math/MathJax.js";
+  
+    var l_config = 'MathJax.Hub.Config({ config: "MathJax.js" }); ' + 'MathJax.Hub.Startup.onload();';
+  
+    if(window.opera) {
+      l_scriptTag.innerHTML = l_config;
+    }
+    else {
+      l_scriptTag.text = l_config;
+    }
+  
+    document.getElementsByTagName("head")[0].appendChild(l_scriptTag);
+  
+  } // initMathJax()
 
   function toggleOverview() {
-
+    
     // If overview visible
     if($g_overview.is(':visible')) {
 
@@ -427,14 +541,14 @@ function superSlides(options) {
     
     // Remove any unnecessary html
     $l_printHtml.find('#MathJax_Hidden, #MathJax_Message, #overview, script, applet').remove();
-    $l_printHtml.find('#slides > div').removeClass('current');
+    $l_printHtml.find('#slides > div').removeClass('current').removeAttr('style');
     
     // Put the doctype + html tag back
     var l_printHtml = '<!doctype html><html lang="en-GB">' + $l_printHtml.html() + '</html>';
     
     // Save the file
-  	$.twFile.save($.twFile.convertUriToLocalPath(document.location.href), l_printHtml);
-  	
+    $.twFile.save($.twFile.convertUriToLocalPath(document.location.href), l_printHtml);
+
 	} // generatePrintHtml()
 
   // **************************************************
@@ -447,25 +561,28 @@ function superSlides(options) {
   log('Current slides: ' + g_currentSlideNumber);
   log('Current view: ' + getCurrentView(g_currentView));
 
-  // Scale slides
-  if(g_currentView == e_view.slides) {
-    scaleSlides();
-  }
+  // Add view class
+  $g_body.addClass(g_currentView == e_view.slides ? 'slides' : 'outline');
 
-  // Init references
+  // Generate initial references
   generateReferences();
+  
+  // Init overview
+  $g_overview = $('<div />').attr('id', 'overview').css('display', 'none').append($('<div />').append($('<div />').addClass('preview'))).appendTo($g_body);
+
+  // Calculate initial scale
+  scaleSlides();
 
   // Show initial slide
   goToSlide(g_currentSlideNumber, e_direction.forward);
 
-  // Init footer
+  // Generate footer
   generateFooters();
-
-  // Init overview
-  $g_overview = $('<div />').attr('id', 'overview').css('display', 'none').append($('<div />').append($('<div />').addClass('preview'))).appendTo('body > .wrapper');
-
-  // Set inital view to slide
-  $g_body.addClass(g_currentView == e_view.slides ? 'slides' : 'outline');
+  
+  // MathJax
+  if(settings.enableMath) {
+    initMathJax();
+  }
 
   // **************************************************
   //
@@ -512,10 +629,17 @@ function superSlides(options) {
             }
             else {
               if(event.keyCode == 32 || event.keyCode == 39) {
-                advance(e_dimension.sub);
+                advance(e_dimension.title);
               }
               else if(event.keyCode == 40) {
-                advance(e_dimension.title);
+                // If reversed sub slides
+                if($g_currentSlide.hasClass('up') ||
+                    ($g_currentSlide.hasClass('sub') && $g_currentSlide.prevAll(':not(.sub)').first().hasClass('up'))) {
+                  previous(e_dimension.sub);
+                }
+                else {
+                  advance(e_dimension.sub);
+                }
               }
             }
             g_skipSlides = '';
@@ -532,10 +656,17 @@ function superSlides(options) {
             }
             else {
               if(event.keyCode == 37) {
-                previous(e_dimension.sub);
+                previous(e_dimension.title);
               }
               else if(event.keyCode == 38) {
-                previous(e_dimension.title);
+                // If reversed sub slides
+                if($g_currentSlide.hasClass('up') ||
+                    ($g_currentSlide.hasClass('sub') && $g_currentSlide.prevAll(':not(.sub)').first().hasClass('up'))) {
+                  advance(e_dimension.sub);
+                }
+                else {
+                  previous(e_dimension.sub);
+                }
               }
             }
             g_skipSlides = '';
